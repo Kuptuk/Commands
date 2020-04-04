8334af37066b8466a45af3979b28b85850051685
Kuptuk <49812392+Kuptuk@users.noreply.github.com>
2020-04-04 22:38:07 +0300

    Update Commands.py

--- Commands.py
+++ Commands.py
@@ -1,8 +1,10 @@
 import telebot
 import pymongo
 
-bot = telebot.TeleBot("1111776586:AAEf9T0hPAcHM7If0vGdnYDAQA9PuebCxs4")
-my_client = pymongo.MongoClient("mongodb+srv://Kuptuk:str38171str@cluster0-qxkw1.gcp.mongodb.net/test?retryWrites=true&w=majority")
+mm = os.environ.get("Mongo")
+tt = os.environ.get("TOKEN")
+my_client = pymongo.MongoClient(mm)
+bot = telebot.TeleBot(tt)
 
 my_database = my_client.Inf
 my_collection = my_database.Inf
